35b274c9062ea11fcf62bd5be79bccd14caf3d04
Lucas <lvagner77@gmail.com>
2021-12-06 11:45:48 +0100

    quasi final

---
+++ frontend/src/components/Widgets/Titre.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Plot from 'react-plotly.js';
+
+
+
+function Titre({accessToken, search})  {
+   
+   
+        return (
+            <div>
+            </div>
+        )
+}
+
+    
+export default Titre;
+    
